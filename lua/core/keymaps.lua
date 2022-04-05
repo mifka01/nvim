d9b5ba6cc4972f93be4ee36f6520b2b57ace3c38
Radim Mifka <radimmifka@gmail.com>
2022-04-05 20:17:07 +0200

    added keymaps for plugins

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,3 +4,16 @@ local map = vim.api.nvim_set_keymap
 map('n', '<space>', '<Nop>', {noremap = true})
 vim.g.mapleader = ' '
 
+
+-- Telescope keymaps
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true})
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true})
+
+
+-- Trouble keymaps
+map('n', '<leader>xx', '<cmd>TroubleToggle<cr>', {noremap = true})
+map('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', {noremap = true})
+
+
