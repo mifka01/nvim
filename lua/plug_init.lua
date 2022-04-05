68436bf75fa2e1f3e6e7b1416ea4e76d3ae52c44
Radim Mifka <radimmifka@gmail.com>
2022-04-05 20:11:27 +0200

    added Plugs

--- lua/plug_init.lua
+++ lua/plug_init.lua
@@ -2,5 +2,25 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/AppData/Local/nvim/lua/plugins/plugged')
 
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'wbthomason/packer.nvim'
+Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/nvim-lsp-installer'
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'folke/lsp-colors.nvim'
+Plug 'folke/trouble.nvim'
+
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+
+Plug 'morhetz/gruvbox'
+
 
 vim.call('plug#end')
