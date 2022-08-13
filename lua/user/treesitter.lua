3c9ff13d9f579447b699498808a8bcb349c07dea
Radim Mifka <radimmifka@gmail.com>
2022-08-13 02:16:12 +0200

    Add plugins

---
+++ lua/user/treesitter.lua
@@ -0,0 +1,38 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
+
+configs.setup({
+    ensure_installed = {
+        "c",
+        "lua",
+        "php",
+        "python",
+        "json",
+        "javascript",
+        "sql",
+        "yaml",
+        "typescript",
+        "vim",
+        "css",
+        "go",
+        "html",
+        "java",
+        "regex",
+    },
+    sync_install = false,
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "css" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+    },
+    autopairs = {
+        enable = true,
+    },
+    indent = { enable = true, disable = { "yaml" } },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+    },
+})
