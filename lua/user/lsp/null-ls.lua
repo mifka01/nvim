62a591e2539de14955c7e413fa1198b0f9d0f0e1
Radim Mifka <radimmifka@gmail.com>
2023-07-21 11:38:38 +0200

    Add mason and phpcsfixer rules

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -3,12 +3,9 @@ if not null_ls_status_ok then
 	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup({
 	debug = false,
 	on_attach = function(client, bufnr)
@@ -29,14 +26,17 @@ null_ls.setup({
 			extra_filetypes = { "toml" },
 			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
 		}),
+
 		-- Python
 		formatting.autopep8,
 		formatting.isort,
 		diagnostics.flake8,
 
 		-- PHP
-		formatting.phpcsfixer,
-		diagnostics.php,
+		formatting.phpcsfixer.with({
+			extra_args = { "--rules=@Symfony" },
+		}),
+		diagnostics.phpcs,
 
 		-- LUA
 		formatting.stylua,
