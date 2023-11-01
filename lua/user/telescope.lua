5d4a5445b09355f57e34ab9251da774ed30cb91e
Radim Mifka <radimmifka@gmail.com>
2023-11-01 01:38:07 +0100

    Add telescope settings

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -8,5 +8,13 @@ telescope.setup({
 		prompt_prefix = " ",
 		selection_caret = " ",
 		path_display = { "smart" },
+		theme = "center",
+		sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				width = 0.9,
+				preview_width = 0.4,
+			},
+		},
 	},
 })
