5fc1b334251cee1f52825d779c8a9c393b5f2ceb
Radim Mifka <radimmifka@gmail.com>
2023-11-01 20:40:19 +0100

    Fix foldingRange

--- lua/user/ufo.lua
+++ lua/user/ufo.lua
@@ -6,10 +6,25 @@ end
 vim.keymap.set("n", "zR", ufo.openAllFolds)
 vim.keymap.set("n", "zM", ufo.closeAllFolds)
 
-ufo.setup({
+local status_ok, lsp_config = pcall(require, "lspconfig")
+if not status_ok then
+	return
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
 	dynamicRegistration = false,
 	lineFoldingOnly = true,
-})
+}
+
+local language_servers = lsp_config.util.available_servers()
+for _, ls in ipairs(language_servers) do
+	lsp_config[ls].setup({
+		capabilities = capabilities,
+	})
+end
+
+ufo.setup()
 
 local status_ok, statuscol = pcall(require, "statuscol")
 if not status_ok then
