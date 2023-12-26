95eb732df033a65269b800687cd913ff5353c83b
Radim Mifka <radimmifka@gmail.com>
2023-12-26 14:38:38 +0100

    Change options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -6,6 +6,7 @@ local options = {
 
 	hlsearch = true, -- highlight all matches on previous search pattern
 	incsearch = true,
+	inccommand = "split",
 
 	ignorecase = true, -- ignore case in search patterns
 	mouse = "a", -- allow the mouse to be used in neovim
@@ -33,7 +34,7 @@ local options = {
 
 	cursorline = true, -- highlight the current line
 	number = true, -- set numbered lines
-	relativenumber = false, -- set relative numbered lines
+	relativenumber = true, -- set relative numbered lines
 	numberwidth = 4, -- set number column width to 2 {default 4}
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 	wrap = false, -- display lines as one long line
