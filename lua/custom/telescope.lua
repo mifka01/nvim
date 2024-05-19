1a636be844e3cc175e6d6553a7cf63b4c2192b7d
Radim Mifka <radimmifka@gmail.com>
2024-05-19 13:50:25 +0200

    feat: fb to browse buffers

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -18,6 +18,7 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<space>ff", builtin.find_files)
 vim.keymap.set("n", "<space>fh", builtin.help_tags)
+vim.keymap.set("n", "<space>fb", builtin.buffers)
 vim.keymap.set("n", "<space>fg", builtin.live_grep)
 vim.keymap.set("n", "<space>/", builtin.current_buffer_fuzzy_find)
 
