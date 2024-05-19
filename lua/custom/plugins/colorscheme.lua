bdc136071d100c60d50c901941aa7b9d3f6dc48c
Radim Mifka <radimmifka@gmail.com>
2024-05-19 13:51:49 +0200

    feat: remove transparency

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,16 +2,15 @@
 -- { "mcchrish/zenbones.nvim", dependencies = { "rktjmp/lush.nvim" } },
 return {
 	{
-
 		"aktersnurra/no-clown-fiesta.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = { transparent = true },
+		-- opts = { transparent = true },
 		config = function()
 			vim.cmd.colorscheme("no-clown-fiesta")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 		end,
 	},
 }
