99b40781e8bc0bc03dfba154ce15c7f9aaba3f9c
Radim Mifka <radimmifka@gmail.com>
2025-01-09 22:00:23 +0100

    fix: remove smart-open

--- lua/custom/telescope.lua
+++ lua/custom/telescope.lua
@@ -2,11 +2,6 @@ local data = assert(vim.fn.stdpath("data")) --[[@as string]]
 
 require("telescope").setup({
 	extensions = {
-		smart_open = {
-			cwd_only = true,
-			match_algorithm = "fzf",
-			disable_devicons = false,
-		},
 		fzf = {},
 		wrap_results = true,
 		history = {
@@ -20,9 +15,8 @@ pcall(require("telescope").load_extension, "fzf")
 pcall(require("telescope").load_extension, "smart_history")
 
 local builtin = require("telescope.builtin")
-local smart_open = require("telescope").extensions.smart_open
 
-vim.keymap.set("n", "<leader>ff", smart_open.smart_open)
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
 vim.keymap.set("n", "<leader>fh", builtin.help_tags)
 vim.keymap.set("n", "<leader>fb", builtin.buffers)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep)
